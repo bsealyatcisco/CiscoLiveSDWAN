7f003cfed4f7e060b9277026870d8dd21d968905
Benjamin Sealy <bsealy@cisco.com>
2020-01-23 08:46:45 -0500

    benscript.py updated for CLEU20

--- benscript.py
+++ benscript.py
@@ -1,11 +1,12 @@
 #####################################################################
 #Cisco SDWAN Lab CRS2000 Reset Script                               #
-#Date: June 2019                                                    #
-#Author: Ben Sealy (bsealy@cisco.com                                #
+#Date: June 2019 Edited: Jan 2020                                   #
+#Author: Ben Sealy (bsealy@cisco.com)                               #
 #####################################################################
 import requests
+from requests.packages import urllib3
 import json
-import urllib3
+#import urllib3
 import re
 import os
 import time
@@ -14,29 +15,35 @@ import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-print 'Sit Back - Enjoy the Ride - This will take about 1 minute to complete'  
+print ('Sit Back - Enjoy the Ride - This will take about 1 minute to complete')
 ##################################################################################################################################
 #Connection Information for SDWAN 2000 dCloud Environment
 ##################################################################################################################################
-vmanage_ip = '198.18.1.10:8443' 
+vmanage_ip = '198.18.1.10:8443'
 vmanage_pw = 'cisco.123'
 request_url = '/j_security_check'
 sess = requests.session()
 r = sess.post('https://' + vmanage_ip + request_url, verify=False, auth=('admin',vmanage_pw))
-##################################################################################################################################   
+##################################################################################################################################
 #Determine input values using student number
 ##################################################################################################################################
 BR2_EDGE_uuid = ''
 BR2_VEDGE_name = ('BR2-VEDGE')
+BR1_VEDGE1_uuid = ''
+BR1_VEDGE1_name = ('BR1-VEDGE1')
 Centralized_Policy_name = ('Centralized-Policy')
 VPN40_IPSEC_DATA = ('VPN40-IPSEC-DATA')
 VPN10_Service_Insertion = ('VPN10-Service-Insertion')
 VPN20_Hub_Spoke = ('VPN20-Hub-Spoke')
+BR1_NAT_Policy = ('BR1-NAT-Policy')
 ##################################################################################################################################
 #Device templates
 ##################################################################################################################################
 BR2_VEDGE_template = ('vEdge_Single_Router_Branch_Guest_IPSec_V01')
-BR2_VEDGE_deleted_template = ('vEdge_Single_Router_Branch_Guest_IPSec_V02')
+    #BR2_VEDGE_deleted_template = ('vEdge_Single_Router_Branch_Guest_IPSec_V02')
+BR2_VEDGE_BGP_template = ('vEdge_Single_Router_Branch_Guest_IPSec_BGP_Peer')
+BR1_VEDGE1_template = ('vEdge_Dual_Router_V01')
+BR1_VEDGE1_NAT_template = ('vEdge_Dual_Router_Service_Side_NAT')
 ##################################################################################################################################
 #Retrieve UUIDs for vedges
 ##################################################################################################################################
@@ -50,11 +57,17 @@ try:
             and entry['host-name'] == BR2_VEDGE_name
         )
     )['uuid']
+    BR1_VEDGE1_uuid = next(
+        (entry for entry in data
+            if 'host-name' in entry
+            and entry['host-name'] == BR1_VEDGE1_name
+        )
+    )['uuid']
 except StopIteration:
     pass
 ##################################################################################################################################
 ##################################################################################################################################
-print 'Device UUID tasks done, processing for 2 seconds'
+print ('Device UUID tasks done, processing for 2 seconds')
 for remaining in range(2, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -105,7 +118,7 @@ r = sess.get('https://' + vmanage_ip + request_url, verify=False, auth=('admin',
 datapolicy_list_json = r.json()
 datapolicy_list = datapolicy_list_json['data']
 
-print 'Get Lists tasks done, processing for 2 seconds'
+print ('Get Lists tasks done, processing for 2 seconds')
 for remaining in range(2, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -123,16 +136,29 @@ Test_Policy_policyId = "False"
 Control_VPN20_defId = "False"
 Control_VPN10_defId = "False"
 Data_VPN40_defId = "False"
+Data_BR1_defId = "False"
 BR2_VEDGE_deleted_templateId = "False"
+BR2_VEDGE_BGP_templateId = "False"
+BR1_VEDGE1_templateId = "False"
+BR1_VEDGE1_NAT_templateId = "False"
 ##################################################################################################################################
 #set/get template ID
 ##################################################################################################################################
 for idx, val in enumerate(template_list):
     if val['templateName'] == BR2_VEDGE_template:
         BR2_VEDGE_templateId = val['templateId']
+# for idx, val in enumerate(template_list):
+#     if val['templateName'] == BR2_VEDGE_deleted_template:
+#         BR2_VEDGE_deleted_templateId = val['templateId']
 for idx, val in enumerate(template_list):
-    if val['templateName'] == BR2_VEDGE_deleted_template:
-        BR2_VEDGE_deleted_templateId = val['templateId']
+    if val['templateName'] == BR2_VEDGE_BGP_template:
+        BR2_VEDGE_BGP_templateId = val['templateId']
+for idx, val in enumerate(template_list):
+    if val['templateName'] == BR1_VEDGE1_template:
+        BR1_VEDGE1_templateId = val['templateId']
+for idx, val in enumerate(template_list):
+    if val['templateName'] == BR1_VEDGE1_NAT_template:
+        BR1_VEDGE1_NAT_templateId = val['templateId']
 ##################################################################################################################################
 #set/get policy ID
 ##################################################################################################################################
@@ -140,19 +166,22 @@ for idx, val in enumerate(centpolicy_list):
     if val['policyName'] == Centralized_Policy_name:
         Centralized_Policy_policyId = val['policyId']
 ##################################################################################################################################
-#set/get def ID  
-###################################################################################################################################     
+#set/get def ID
+###################################################################################################################################
 for idx, val in enumerate(controlpolicy_list):
     if val['name'] == VPN20_Hub_Spoke:
        Control_VPN20_defId = val['definitionId']
 for idx, val in enumerate(controlpolicy_list):
     if val['name'] == VPN10_Service_Insertion:
-       Control_VPN10_defId = val['definitionId']      
+       Control_VPN10_defId = val['definitionId']
 for idx, val in enumerate(datapolicy_list):
     if val['name'] == VPN40_IPSEC_DATA:
-       Data_VPN40_defId = val['definitionId']  
+       Data_VPN40_defId = val['definitionId']
+for idx, val in enumerate(datapolicy_list):
+    if val['name'] == BR1_NAT_Policy:
+       Data_BR1_defId = val['definitionId']
 ##################################################################################################################################
-print 'Indexing tasks done, processing for 2 seconds'
+print ('Indexing tasks done, processing for 2 seconds')
 for remaining in range(2, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -171,10 +200,32 @@ r = sess.post('https://' + vmanage_ip + request_url, verify=False, auth=('admin'
 if r.status_code is 200:
     print (BR2_VEDGE_template + ' has been attached to ' + BR2_VEDGE_name)
 else:
-    print r.status_code
-    print 'Template failed to attach to ' + BR2_VEDGE_name
+    print (r.status_code)
+    print ('Template failed to attach to ' + BR2_VEDGE_name)
 
-print 'BRANCH2 Template Attach done, processing for 10 seconds'
+print ('BRANCH2 Template Attach done, processing for 10 seconds')
+for remaining in range(10, 0, -1):
+    sys.stdout.write("\r")
+    sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+    sys.stdout.flush()
+    time.sleep(1)
+
+sys.stdout.write("\rComplete!            \n")
+##################################################################################################################################
+#Reattach V01 Device Template to Br1-vedge1 Template
+    #Get list of device template objects
+##################################################################################################################################
+headers = {'Content-Type': 'application/json', 'cache-control': 'no-cache', 'accept': '*/*', 'accept-encoding': 'gzip, deflate'}
+request_url = '/dataservice/template/device/config/attachfeature/'
+body = json.loads(open('./BR1_1_Template_V01.json').read().replace("$BR1_VEDGE1_templateId", BR1_VEDGE1_templateId).replace("$BR1_VEDGE1_uuid", BR1_VEDGE1_uuid))
+r = sess.post('https://' + vmanage_ip + request_url, verify=False, auth=('admin',vmanage_pw,), json=body, headers=headers)
+if r.status_code is 200:
+    print (BR1_VEDGE1_template + ' has been attached to ' + BR1_VEDGE1_name)
+else:
+    print (r.status_code)
+    print ('Template failed to attach to ' + BR1_VEDGE1_name)
+
+print ('BRANCH1 VEDGE1 Template Attach done, processing for 10 seconds')
 for remaining in range(10, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -185,31 +236,31 @@ sys.stdout.write("\rComplete!            \n")
 ##################################################################################################################################
 #Delete Templates
 ##################################################################################################################################
-if BR2_VEDGE_templateId is "False":
-    print BR2_VEDGE_deleted_template + ' does not exist or is misnamed. Please manually inspect the Device Templates'
-else:
-    request_url = '/dataservice/template/device/' + BR2_VEDGE_deleted_templateId
-    r = sess.delete('https://' + vmanage_ip + request_url, verify=False, auth=('admin',vmanage_pw))
-    if r.status_code is 200:
-        print (BR2_VEDGE_deleted_template + ' has been deleted.')
-    else:
-        print r.status_code
-        print 'Failed to delete ' + BR2_VEDGE_deleted_template
+# if BR2_VEDGE_templateId is "False":
+#     print BR2_VEDGE_deleted_template + ' does not exist or is misnamed. Please manually inspect the Device Templates'
+# else:
+#     request_url = '/dataservice/template/device/' + BR2_VEDGE_deleted_templateId
+#     r = sess.delete('https://' + vmanage_ip + request_url, verify=False, auth=('admin',vmanage_pw))
+#     if r.status_code is 200:
+#         print (BR2_VEDGE_deleted_template + ' has been deleted.')
+#     else:
+#         print r.status_code
+#         print 'Failed to delete ' + BR2_VEDGE_deleted_template
 ##################################################################################################################################
-#Deactivate 
+#Deactivate
 ##################################################################################################################################
 if Centralized_Policy_policyId is "False":
-    print Centralized_Policy_name + ' does not exist or is misnamed. Please manually inspect the central policy'
+    print (Centralized_Policy_name + ' does not exist or is misnamed. Please manually inspect the central policy')
 else:
     request_url = '/dataservice/template/policy/vsmart/deactivate/' + Centralized_Policy_policyId + '?confirm=true'
     r = sess.post('https://' + vmanage_ip + request_url, verify=False, auth=('admin',vmanage_pw))
     if r.status_code is 200:
         print (Centralized_Policy_name + ' has been deactivated.')
     else:
-        print r.status_code
-        print 'Failed to deactivate ' + Centralized_Policy_name
+        print (r.status_code)
+        print ('Failed to deactivate ' + Centralized_Policy_name)
 
-print 'Centralized Policy deactivation done, processing for 30 seconds'
+print ('Centralized Policy deactivation done, processing for 30 seconds')
 for remaining in range(30, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -221,7 +272,7 @@ sys.stdout.write("\rComplete!            \n")
 #Delete Policy
 ##################################################################################################################################
 if Centralized_Policy_policyId is "False":
-    print Centralized_Policy_name + ' does not exist or is misnamed. Please manually inspect the central policy'
+    print (Centralized_Policy_name + ' does not exist or is misnamed. Please manually inspect the central policy')
 else:
     print (Centralized_Policy_policyId)
     request_url = '/dataservice/template/policy/vsmart/' + Centralized_Policy_policyId
@@ -229,10 +280,10 @@ else:
     if r.status_code is 200:
         print (Centralized_Policy_name + ' has been deleted.')
     else:
-        print r.status_code
-        print 'Failed to delete ' + Centralized_Policy_name
+        print (r.status_code)
+        print ('Failed to delete ' + Centralized_Policy_name)
 
-print 'Centralized Policy deletion done, processing for 10 seconds'
+print ('Centralized Policy deletion done, processing for 10 seconds')
 for remaining in range(10, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -244,7 +295,7 @@ sys.stdout.write("\rComplete!            \n")
 #Delete control and data policy definitions
 ##################################################################################################################################
 if Control_VPN20_defId is "False":
-    print VPN20_Hub_Spoke + ' does not exist or is misnamed. Please manually inspect the traffic policy'
+    print (VPN20_Hub_Spoke + ' does not exist or is misnamed. Please manually inspect the traffic policy')
 else:
     print (Control_VPN20_defId)
     request_url = '/dataservice/template/policy/definition/control/' + Control_VPN20_defId
@@ -252,10 +303,10 @@ else:
     if r.status_code is 200:
         print (VPN20_Hub_Spoke + ' has been deleted.')
     else:
-        print r.status_code
-        print 'Failed to delete ' + VPN20_Hub_Spoke
+        print (r.status_code)
+        print ('Failed to delete ' + VPN20_Hub_Spoke)
 ##################################################################################################################################
-print 'VPN20 Policy deletion done, processing for 2 seconds'
+print ('VPN20 Policy deletion done, processing for 2 seconds')
 for remaining in range(2, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -265,7 +316,7 @@ for remaining in range(2, 0, -1):
 sys.stdout.write("\rComplete!            \n")
 ##################################################################################################################################
 if Control_VPN10_defId is "False":
-    print VPN10_Service_Insertion + ' does not exist or is misnamed. Please manually inspect the traffic policy'
+    print (VPN10_Service_Insertion + ' does not exist or is misnamed. Please manually inspect the traffic policy')
 else:
     print (Control_VPN10_defId)
     request_url = '/dataservice/template/policy/definition/control/' + Control_VPN10_defId
@@ -273,10 +324,10 @@ else:
     if r.status_code is 200:
         print (VPN10_Service_Insertion + ' has been deleted.')
     else:
-        print r.status_code
-        print 'Failed to delete ' + VPN10_Service_Insertion
+        print (r.status_code)
+        print ('Failed to delete ' + VPN10_Service_Insertion)
 ##################################################################################################################################
-print 'VPN10 Policy deletion done, processing for 2 seconds'
+print ('VPN10 Policy deletion done, processing for 2 seconds')
 for remaining in range(2, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -286,7 +337,7 @@ for remaining in range(2, 0, -1):
 sys.stdout.write("\rComplete!            \n")
 ##################################################################################################################################
 if Data_VPN40_defId is "False":
-    print VPN40_IPSEC_DATA + ' does not exist or is misnamed. Please manually inspect the traffic policy'
+    print (VPN40_IPSEC_DATA + ' does not exist or is misnamed. Please manually inspect the traffic policy')
 else:
     print (Data_VPN40_defId)
     request_url = '/dataservice/template/policy/definition/data/' + Data_VPN40_defId
@@ -294,10 +345,10 @@ else:
     if r.status_code is 200:
         print (VPN40_IPSEC_DATA + ' has been deleted.')
     else:
-        print r.status_code
-        print 'Failed to delete ' +VPN40_IPSEC_DATA  
+        print (r.status_code)
+        print ('Failed to delete ' + VPN40_IPSEC_DATA)
 ##################################################################################################################################
-print 'VPN40 Policy deletion done, processing for 2 seconds'
+print ('VPN40 Policy deletion done, processing for 2 seconds')
 for remaining in range(2, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} seconds remaining.".format(remaining))
@@ -306,4 +357,26 @@ for remaining in range(2, 0, -1):
 
 sys.stdout.write("\rComplete!            \n")
 ##################################################################################################################################
-print 'SCRIPT EOF'  
+##################################################################################################################################
+if Data_BR1_defId is "False":
+    print (BR1_NAT_Policy + ' does not exist or is misnamed. Please manually inspect the traffic policy')
+else:
+    print (Data_BR1_defId)
+    request_url = '/dataservice/template/policy/definition/data/' + Data_BR1_defId
+    r = sess.delete('https://' + vmanage_ip + request_url, verify=False, auth=('admin',vmanage_pw))
+    if r.status_code is 200:
+        print (BR1_NAT_Policy + ' has been deleted.')
+    else:
+        print (r.status_code)
+        print ('Failed to delete ' + BR1_NAT_Policy)
+##################################################################################################################################
+print ('VPN40 Policy deletion done, processing for 2 seconds')
+for remaining in range(2, 0, -1):
+    sys.stdout.write("\r")
+    sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+    sys.stdout.flush()
+    time.sleep(1)
+
+sys.stdout.write("\rComplete!            \n")
+##################################################################################################################################
+print ('SCRIPT EOF')
